723ea336f1f5853bd93b9ea72734c1a48e53f7b5
Thiago Vinhas <vinhas.thiago@gmail.com>
2025-03-18 13:32:34 +0000

    add database H2 to the project

---
+++ src/main/java/com/example/cashcard/CashCardController.java
@@ -0,0 +1,31 @@
+package com.example.cashcard;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/cashcards")
+public class CashCardController {
+    private final CashCardRepository cashCardRepository;
+
+    public CashCardController(CashCardRepository cashCardRepository) {
+        this.cashCardRepository = cashCardRepository;
+    }
+
+    @GetMapping("/{requestedId}")
+    private ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
+        Optional<CashCard> cashCardOptional = cashCardRepository.findById(requestedId);
+
+        if (cashCardOptional.isPresent()) {
+            CashCard cashCard = new CashCard(99L, 123.45);
+            return ResponseEntity.ok(cashCard);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
